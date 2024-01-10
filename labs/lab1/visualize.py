5cc5132b4f4029c48c49c7712f23fc949d60bbb5
JakeRusso3 <110993794+JakeRusso3@users.noreply.github.com>
2024-01-09 19:07:17 -0500

    Update visualize.py

--- labs/lab1/visualize.py
+++ labs/lab1/visualize.py
@@ -66,7 +66,7 @@ def show_all_FK(state):
 configurations = [
     np.array([ 0,    0,     0, -pi/2,     0, pi/2, pi/4 ]),
     np.array([ 0,    0,     0, -0.07,     0,    0,    0 ]),
-    np.array([ pi/2, pi/2,  0,     0,     0,    0,    0 ]),
+    np.array([ pi/2, pi/2,  0, -0.07,     0,    0,    0 ]),
     np.array([ 0,    0,    pi, -pi/2,   2.7, pi/2, pi/4 ]),
     np.array([ 0,-pi/4,     0,-3*pi/4,  2.7, pi/2, pi/4 ]),
 ]

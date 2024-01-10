7c5d4b8e0d0230aa600bc6405403c6e625c37af1
JakeRusso3 <110993794+JakeRusso3@users.noreply.github.com>
2024-01-09 22:01:48 -0500

    Update demo.py

--- labs/lab0/demo.py
+++ labs/lab0/demo.py
@@ -26,10 +26,10 @@ q = arm.neutral_position()
 arm.safe_move_to_position(q)
 arm.open_gripper()
 
-# q = np.array([0,0 ,0,0,0,0,0])
- q = np.array([ pi/2, pi/2,  0, -0.07,     0,    0,    0 ]),
-# q = np.array([ 0,    0,  2.89, -pi/2,   2.7, pi/2, pi/4 ]),
-# q = np.array([ 0,-pi/4,     0,-3*pi/4,  2.7, pi/2, pi/4 ]),
+#q = np.array([0,0 ,0,0,0,0,0])
+q = np.array([ pi/2, pi/2,  0, -0.07,     0,    0,    0 ]),
+#q = np.array([ 0,    0,  2.89, -pi/2,   2.7, pi/2, pi/4 ]),
+#q = np.array([ 0,-pi/4,     0,-3*pi/4,  2.7, pi/2, pi/4 ]),
 # TODO: try changing this!
 arm.safe_move_to_position(q)
 arm.close_gripper()

12831f3dd80b6959ac9aef4b9dfb632d8fd21a36
JakeRusso3 <110993794+JakeRusso3@users.noreply.github.com>
2024-01-10 00:36:06 -0500

    Update calcJacobian.py

--- lib/calcJacobian.py
+++ lib/calcJacobian.py
@@ -9,10 +9,23 @@ def calcJacobian(q_in):
     rows correspond to the linear velocity and the last three rows correspond to
     the angular velocity, expressed in world frame coordinates
     """
+    fk = FK()
+    joint_positions, T0e = fk.forward(q_in)
+    end_eff_position = T0e[:3,3]
+    print(end_eff_position)
+    end_eff_rotation_matrix = T0e [:3,:3]
+    print(end_eff_rotation_matrix)
+    All_T_matrices = fk.get_T(q_in)
+    Rotation_matrices = [T0x[:3,:3] for T0x in All_T_matrices)
 
-    J = np.zeros((6, 7))
-
-    ## STUDENT CODE GOES HERE
+    axis_of_rotation = fk.get_axis_of_rotation(q_in)
+    
+    finaljp = joint_positions[-1, :]
+    for ii in range(7)
+        for i in range(7):
+        r = np.array(finaljp - joint_positions[ii, :])
+        J[:3, i] = np.cross(axis_of_rotations[:, i], r)
+        J[3:, i] = axis_of_rotations[:, i]
 
     return J
 
